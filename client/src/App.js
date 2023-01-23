75af65809e08612be8bd6619017d15df1898bbca
lovecodesuraj <chaudharysuraj2252@gmail.com>
2023-01-23 11:44:55 +0530

    p1.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {Container,AppBar,Typography,Grow,Grid} from "@material-ui/core";
+import AddQuestion from './components/forms/question/question';
+import Questions from './components/questions/questions';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return<>
+     <Container maxWidth="lg">
+      <AppBar position='static' color='inherit'>
+         <Typography variant='h2' align='center'>Medverse</Typography>
+      </AppBar>
+      <Grow in>
+        <Container >
+          <Grid container justify="space-between" alignItems='stretch' spacing={3} >
+            <Grid item xs={12} sm={7}>
+               <Questions />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <AddQuestion />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+     </Container>
+  </>
 }
 
 export default App;
---
+++ client/src/components/questions/questions.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Questions =()=>{
+    return<>
+    </>
+}
+
+export default Questions;
